3ea84fc301e5ea46927d1212920fff040dba3311
Rory Duthie <roryduthie@Rorys-MBP.lan>
2020-12-13 17:10:54 +0000

    Change port

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 source venv/bin/activate
-exec gunicorn -b :5000 --access-logfile - --error-logfile - app --timeout 300
+exec gunicorn -b :8400 --access-logfile - --error-logfile - app --timeout 300
